33bcc53024a9f4fe1c2e69c0e2fb124031fd1d41
Jamie Morgan <jamiemorgan.dev@gmail.com>
2017-05-14 01:45:48 +0100

    Made a texture class, linked opengl + glew

--- DeliciousEngine/std_cvars.h
+++ DeliciousEngine/std_cvars.h
@@ -6,6 +6,8 @@
 
 std::vector<console_var> standard_cvars = {
 	{ "vid_init", 0, VAR_BOOL, CVAR_SYSTEM},
+	{ "log_init", 0, VAR_BOOL, CVAR_SYSTEM},
+
 	{ "vid_width", 1280, VAR_INT, CVAR_USER},
 	{ "vid_height", 720, VAR_INT, CVAR_USER},
 	{ "vid_fullscreen", 0, VAR_BOOL, CVAR_USER},
---
+++ DeliciousEngine/texture.h
@@ -0,0 +1,12 @@
+#ifndef DELICIOUS_TEXTURE_H
+#define DELICIOUS_TEXTURE_H
+
+#include <GL/GL.h>
+#include "dtypes.h"
+
+class Texture {
+public:
+
+};
+
+#endif
