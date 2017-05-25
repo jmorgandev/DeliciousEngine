51a3fd8cbad820f5409b0802fd54a4bfdeb8ed0d
Jamie Morgan <jamiemorgan.dev@gmail.com>
2017-05-26 00:26:57 +0100

    Wrote more texture stuff

--- DeliciousEngine/texture.h
+++ DeliciousEngine/texture.h
@@ -1,12 +1,24 @@
 #ifndef DELICIOUS_TEXTURE_H
 #define DELICIOUS_TEXTURE_H
 
-#include <GL/GL.h>
+#include <GL/glew.h>
 #include "dtypes.h"
 
 class Texture {
 public:
+	Texture(GLuint object_in, uint16 width_in, uint16 height_in, uint8 bpp_in);
+	~Texture();
 
+	void	bind();
+
+	GLuint	get_gpu_object();
+	uint16	get_width();
+	uint16	get_height();
+	uint8	get_bpp();
+private:
+	GLuint	gpu_object;
+	uint16	width, height;
+	uint8	bpp;
 };
 
 #endif
