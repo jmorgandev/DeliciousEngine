473eb5f4a6d79d5b9b24f3ce5aa9a2be28e984d3
Jamie Morgan <jamiemorgan.dev@gmail.com>
2017-11-30 20:17:02 +0000

    some notes...

--- DeliciousEngine/main.cpp
+++ DeliciousEngine/main.cpp
@@ -13,3 +13,56 @@ int main(char** argv, int argc) {
 
 	return 0;
 }
+
+/* ==== NOTES ====
+
+FMOD vs SDL_Mixer vs Wwise
+
+Multithreading:
+    -locks
+	-mutexes
+	-condition variables
+
+Newton dynamics
+
+Advanced audio effects:
+	-materials
+	-pathfinding
+	-special effects
+
+Rendering:
+	-deferred rendering
+	-per-pixel lighting
+	-shadow mapping
+	-reflections
+	-framebuffer effects
+	-lightmapping
+
+Angelscript vs Lua vs Python
+
+Reloading C++ .dlls?
+
+Hotloading resources:
+	-sounds
+	-meshes
+	-textures
+	-shaders
+
+Custom extension workflows:
+	-"FileLoaders"?
+	-script extensions
+
+Framerate/Timing:
+	-FPS cap?
+	-Vsync?
+	-Determinism?
+	-Different "timelines"
+	-Delta time?
+	-Physics time?
+	-Fixed time updates?
+
+Skinned animation? FBX? other animation formats?
+
+User-accessible GUI?
+
+*/
