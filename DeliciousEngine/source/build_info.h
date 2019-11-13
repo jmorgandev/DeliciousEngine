fb39026d9a903fd07cd39d2f5ac7a1ab5decc055
Jamie Morgan <jamiemorgan.dev@gmail.com>
2019-11-13 18:34:16 +0000

    Factor some engine build settings into build_settings.h

--- DeliciousEngine/source/build_info.h
+++ DeliciousEngine/source/build_info.h
@@ -2,26 +2,18 @@
 #define DELICIOUS_BUILD_INFO_H
 
 #include "dcm.h"
-
-//Main version declaration
-#define ENG_VER_MAJOR 0
-#define ENG_VER_MINOR 1
-#define ENG_VER_PATCH 1
+#include "build_settings.h"
 
 #define ENG_VERSION_STR \
 	TO_STR(ENG_VER_MAJOR) "." TO_STR(ENG_VER_MINOR) "." TO_STR(ENG_VER_PATCH)
 
 #define DEFAULT_WIN_TITLE "Delicious Engine " ENG_VERSION_STR
 
-#define ENGINE_GL_MAJOR 4
-#define ENGINE_GL_MINOR 3
-
-#define DEFAULT_GLSL_VERSION 430
 #define DEFAULT_GLSL_STR "#version " TO_STR(DEFAULT_GLSL_VERSION) "\n"
 
-#define USE_SCRIPTING false
+/*==================== CHANGELOG (0.1.2) =============================
 
-/*============================ CHANGELOG =============================
+ * Removed scripting, focus on cpp interface
 
  * Changed console command registering to support lua functions
 
@@ -33,13 +25,9 @@
 
  * sol2 lua binds for resource loading
 
- * Basic time keeping inside World system
 
  *====================================================================
 */
-
-//@Bug: Console scrolling does not work properly, clips strings too early...
-//		This sometimes doesn't even happen, what gives?
 
 //@Bug: Material uniform block does not update dynamically?
 
--- DeliciousEngine/source/build_settings.h
+++ DeliciousEngine/source/build_settings.h
@@ -1,13 +1,22 @@
 #ifndef DELICIOUS_BUILD_SETTINGS_H
 #define DELICIOUS_BUILD_SETTINGS_H
 
+//Main version declaration
+#define ENG_VER_MAJOR 0
+#define ENG_VER_MINOR 1
+#define ENG_VER_PATCH 1
+
+#define ENGINE_GL_MAJOR 4
+#define ENGINE_GL_MINOR 4
+#define DEFAULT_GLSL_VERSION 440
+
 #define MAX_ENTITIES 786
 
-#define USE_MEMORY_LIMIT false
+#define USE_MEMORY_LIMIT 0
 #define MEMORY_LIMIT 1024 * 1024 * 1024 // 1 GB
 
 #define CON_MAX_NAME 32
 
-
+#define USE_SCRIPTING 0
 
 #endif
