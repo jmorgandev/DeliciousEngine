9e4e5af2172d53e7b76de1793a82dbb715deb0e2
Jamie Morgan <jamiemorgan.dev@gmail.com>
2019-11-13 18:50:57 +0000

    add some platform specific type definitions

--- DeliciousEngine/source/dtypes.h
+++ DeliciousEngine/source/dtypes.h
@@ -1,51 +1,26 @@
 #ifndef DTYPES_H
 #define DTYPES_H
 
-#include <stdint.h>
+typedef unsigned __int64 uint64, qword;
+typedef __int64 int64;
+typedef unsigned __int32 uint32, dword;
+typedef __int32 int32;
+typedef unsigned __int16 uint16, word;
+typedef __int16 int16;
+typedef unsigned __int8 uint8, uchar, byte;
+typedef __int8 int8;
 
-typedef uint64_t uint64, qword;
-typedef int64_t int64;
-typedef uint32_t uint32, dword;
-typedef int32_t int32;
-typedef uint16_t uint16, word;
-typedef int16_t int16;
-typedef uint8_t uint8, uchar, byte;
-typedef int8_t int8;
-
+#ifdef BUILD_x64
+typedef int64 sint;
+typedef uint64 uint;
+#elif BUILD_x86
+typedef int32 sint;
+typdef uint32 uint;
+#else
+typedef int sint;
 typedef unsigned int uint;
+#endif
+
 typedef const char *cstring;
-
-template <class type> class scoped_ptr {
-public:
-	scoped_ptr() : raw_pointer(nullptr) {}
-	scoped_ptr(type* pointer) : raw_pointer(pointer) {}
-	~scoped_ptr() { free(); }
-
-	scoped_ptr& operator=(const scoped_ptr& ptr) { raw_pointer = ptr.raw_pointer; return *this; }
-	scoped_ptr& operator=(type* ptr) { raw_pointer = ptr; return *this; }
-	type& operator* () { return *raw_pointer; }
-	type& operator[] (int i) { return raw_pointer[i]; }
-	type* operator-> () { return raw_pointer; }
-	type* raw() { return raw_pointer; }
-	void free() { if (raw_pointer) { delete raw_pointer; raw_pointer = nullptr; } }
-private:
-	type* raw_pointer;
-};
-template <class type> class scoped_array {
-public:
-	scoped_array() : raw_pointer(nullptr) {}
-	scoped_array(type* pointer) : raw_pointer(pointer) {}
-	~scoped_array() { free(); }
-
-	scoped_array& operator=(const scoped_array& ptr) { raw_pointer = ptr.raw_pointer; return *this; }
-	scoped_array& operator=(type* ptr) { raw_pointer = ptr; return *this; }
-	type& operator* () { return *raw_pointer; }
-	type& operator[] (int i) { return raw_pointer[i]; }
-	type* operator-> () { return raw_pointer; }
-	type* raw() { return raw_pointer; }
-	void free() { if (raw_pointer) { delete[] raw_pointer; raw_pointer = nullptr; } }
-private:
-	type* raw_pointer;
-};
 
 #endif
